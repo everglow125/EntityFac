bf253718b349fe0670f22d18ded4a00f39df8383
everglow125 <183900112@qq.com>
2016-04-13 17:49:21 +0800

    添加项目文件

---
+++ EntityFac/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace EntityFac
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtConnection = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.cbxTables = new System.Windows.Forms.CheckedListBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(687, 39);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "查询";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "数据库链接";
+            // 
+            // txtConnection
+            // 
+            this.txtConnection.Location = new System.Drawing.Point(97, 39);
+            this.txtConnection.Name = "txtConnection";
+            this.txtConnection.Size = new System.Drawing.Size(584, 21);
+            this.txtConnection.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(723, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "生成文件";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(106, 45);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(584, 21);
+            this.txtAddress.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 12);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "文件存放地址";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtConnection);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 0);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(913, 100);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "groupBox1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.txtAddress);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox2.Location = new System.Drawing.Point(0, 438);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(913, 100);
+            this.groupBox2.TabIndex = 7;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "groupBox2";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.cbxTables);
+            this.groupBox3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.groupBox3.Location = new System.Drawing.Point(0, 100);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(913, 338);
+            this.groupBox3.TabIndex = 8;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "groupBox3";
+            // 
+            // cbxTables
+            // 
+            this.cbxTables.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cbxTables.FormattingEnabled = true;
+            this.cbxTables.Location = new System.Drawing.Point(3, 17);
+            this.cbxTables.Name = "cbxTables";
+            this.cbxTables.Size = new System.Drawing.Size(907, 318);
+            this.cbxTables.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(16, 20);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(913, 538);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "实体类生成";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtConnection;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.CheckedListBox cbxTables;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ EntityFac/Form1.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace EntityFac
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sql = @"
+SELECT  name AS TableName 
+FROM    sysobjects
+WHERE   xtype = 'U'
+ORDER BY name;";
+            DataTable dt = ExecuteDataTable(this.txtConnection.Text, sql);
+            this.cbxTables.DataSource = dt;
+            this.cbxTables.ValueMember = "TableName";
+            this.cbxTables.DisplayMember = "TableName";
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string sql = @"
+        SELECT  
+                CONVERT(VARCHAR, T1.name) AS ColumnName ,--字段名
+                CONVERT(VARCHAR, T2.name) AS ColumnType ,--字段类型
+                T1.prec AS MaxLength ,--最大长度
+                CONVERT(VARCHAR, T5.COLUMN_DEFAULT) AS DefaultValue ,--默认值
+                CONVERT(VARCHAR, T4.value) AS Comment ,--描述
+                T1.isnullable AS NullAble--是否为空
+        FROM    syscolumns T1
+                LEFT JOIN systypes T2 ON T1.xusertype = T2.xusertype
+                INNER JOIN sysobjects T3 ON T1.id = T3.id
+                                            AND T3.xtype = 'U '
+                                            AND T3.name <> 'dtproperties '
+                LEFT JOIN sys.extended_properties T4 ON T1.id = T4.major_id
+                                                        AND T1.colid = T4.minor_id
+                JOIN INFORMATION_SCHEMA.COLUMNS T5 ON T1.name = T5.COLUMN_NAME
+                                                      AND T5.TABLE_NAME = '{0}'
+        WHERE   T3.name = '{0}';";
+            for (int i = 0; i < cbxTables.CheckedItems.Count; i++)
+            {
+                DataRowView dv = ((DataRowView)cbxTables.CheckedItems[i]);
+                if (dv == null) continue;
+                string id = dv["TableName"].ToString();
+                var temp = ExecuteDataTable(this.txtConnection.Text, string.Format(sql, id));
+                using (var temdp = File.Create("F:/Tmmp/" + id + ".cs"))
+                {
+                }
+                using (StreamWriter sw = new StreamWriter("F:/Tmmp/" + id + ".cs"))
+                {
+                    sw.WriteLine("using System;");
+                    sw.WriteLine("namespace " + "空间");
+                    sw.WriteLine("{");
+                    sw.WriteLine("\tpublic class " + id);
+                    sw.WriteLine("\t{");
+                    foreach (DataRow dr in temp.Rows)
+                    {
+                        sw.WriteLine("\t\t/// <summary>");
+                        sw.WriteLine("\t\t/// " + dr["Comment"].ToString());
+                        sw.WriteLine("\t\t/// <summary>");
+                        sw.WriteLine("\t\tpublic " + dr["ColumnType"] + " " + dr["ColumnName"] + " { get; set; }");
+                        sw.WriteLine("");
+                    }
+                    sw.WriteLine("\t}");
+                    sw.WriteLine("}");
+                }
+            }
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="constr"></param>
+        /// <param name="cmdText"></param>
+        /// <returns></returns>
+        public DataTable ExecuteDataTable(string constr, string cmdText)
+        {
+            try
+            {
+                using (SqlConnection cnn = new SqlConnection(constr))
+                {
+                    cnn.Open();
+                    DataSet ds = new DataSet();
+                    try
+                    {
+                        SqlDataAdapter command = new SqlDataAdapter(cmdText, cnn);
+                        command.Fill(ds, "ds");
+                    }
+                    catch (System.Data.SqlClient.SqlException ex)
+                    {
+                        throw new Exception(ex.Message);
+                    }
+                    if (ds != null && ds.Tables.Count > 0)
+                        return ds.Tables[0];
+                    return null;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < cbxTables.Items.Count; i++)
+            {
+                cbxTables.SetItemCheckState(i, CheckState.Checked);
+            }
+        }
+    }
+}
